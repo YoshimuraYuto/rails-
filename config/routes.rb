52e57a4e3cbb57cc17d90ff30e2d8313078e9200
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-09-23 10:46:40 +0900

    post

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'hellos/index'
+  resources :apps
+  root 'hellos#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
